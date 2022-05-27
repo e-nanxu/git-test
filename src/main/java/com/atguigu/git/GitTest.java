28ba41a57e36cdf2a9a751264412f292ae38f970
e_nanxu <e_nanxu@163.com>
2022-05-27 14:16:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author e_n
+ * @version 1.0.0
+ * @ClassName GitTest.java
+ * @Description
+ * @CreateTime 2022/05/27 14:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
